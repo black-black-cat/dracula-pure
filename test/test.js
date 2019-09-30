f9a1589bf6d8d22c69918d430fd673501b6d743b
LAIHUA_LZK <linzhaokuan@laihua.com>
2019-09-30 11:55:15 +0800

    add template-expression color

--- test/test.js
+++ test/test.js
@@ -52,9 +52,11 @@ var proxy = function (fn, context) {
 };
 
 Sound.play = function () { }
-Sound.prototype = { something; }
+Sound.prototype = { something }
 Sound.prototype.play = function () { }
 Sound.prototype.play = myfunc
 var parser = document.createElement('a');
 parser.href = "http://example.com:3000/pathname/?search=test#hash";
 parser.hostname; // => "example.com"
+
+const temp = `123${parser.href}`
