6e30bd6e10b324af9b8b18e182652fa9c6255b25
black-black-cat <applyk99@163.com>
2020-10-19 23:22:23 +0800

    update colors

--- src/colors/dark-palette.js
+++ src/colors/dark-palette.js
@@ -1,14 +1,31 @@
+const { darken } = require('./utils')
+
+// summer vacation
+// const colors = {
+// 	red:       '#FA5E5B',
+// 	orange:    '#FF953F',
+// 	yellow:    '#FFC83F',
+// 	teal:      '#0FDEBD',
+// 	green:     '#16C98D',
+// 	blueLight: '#70C2FF',
+// 	blueDark:  '#2C98EC',
+// 	purple:    '#B89FF7',
+// 	pinkLight: '#FF98AD',
+// 	pinkDark:  '#FF708E',
+// }
+
+// dracula
 const colors = {
-	red:       '#FA5E5B',
-	orange:    '#FF953F',
-	yellow:    '#FFC83F',
+	red:       '#ff5555',
+	orange:    '#ffb86c',
+	yellow:    darken('#f1fa8c', 30),
 	teal:      '#0FDEBD',
-	green:     '#16C98D',
-	blueLight: '#70C2FF',
+	green:     '#50fa7b',
+	blueLight: '#8be9fd',
 	blueDark:  '#2C98EC',
-	purple:    '#B89FF7',
+	purple:    '#bd93f9',
 	pinkLight: '#FF98AD',
-	pinkDark:  '#FF708E',
+	pinkDark:  '#ff79c6',
 }
 
 module.exports = colors
